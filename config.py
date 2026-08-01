cbd291853d5278fde0910dddf2fbe94f4111f3c2
nadezdaharcikova60-commits <nadezdaharcikova60@gmail.com>
2026-08-01 14:32:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "8794292862:AAGVRhbW_IeDcU5YVUQIg7vs_XLQwC0mJbU"
 ADMIN_ID = 8354533839
-PRICE_PER_STAR = 0.80  # Цена за 1 звезду в грн
+PRICE_PER_STAR = 0.75  # Цена за 1 звезду в грн
 CARD_NUMBER = "4874070072177239"  # Сюда впиши номер своей карты или банка
 SUPPORT_USERNAME = "@Durov_soset_apple_r"  # Или твой реальный юзернейм в Telegram без @
